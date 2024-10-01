03e7655f94821dc64aa4eeae6485f3f8f0df8eea
Naveen <naveen246@gmail.com>
2024-10-01 12:40:07 +0530

    Add comments for config options

--- slatedb/config.go
+++ slatedb/config.go
@@ -12,12 +12,67 @@ const (
 
 // DBOptions Configuration options for the database. These options are set on client startup.
 type DBOptions struct {
-	FlushMS              uint64
+	// How frequently to flush the write-ahead log to object storage (in
+	// milliseconds).
+	//
+	// When setting this configuration, users must consider:
+	//
+	// * **Latency**: The higher the flush interval, the longer it will take for
+	//   writes to be committed to object storage. Writers blocking on `put` calls
+	//   will wait longer for the write. Readers reading committed writes will also
+	//   see data later.
+	// * **API cost**: The lower the flush interval, the more frequently PUT calls
+	//   will be made to object storage. This can increase your object storage costs.
+	//
+	// We recommend setting this value based on your cost and latency tolerance. A
+	// 100ms flush interval should result in $130/month in PUT costs on S3 standard.
+	//
+	// Keep in mind that the flush interval does not include the network latency. A
+	// 100ms flush interval will result in a 100ms + the time it takes to send the
+	// bytes to object storage.
+	FlushMS uint64
+
+	// How frequently to poll for new manifest files. Refreshing the manifest file
+	// allows writers to detect fencing operations and allows readers to detect newly
+	// compacted data.
 	ManifestPollInterval time.Duration
-	MinFilterKeys        uint32
-	L0SSTSizeBytes       uint64
-	CompactorOptions     *CompactorOptions
-	CompressionCodec     CompressionCodec
+
+	// Write SSTables with a bloom filter if the number of keys in the SSTable
+	// is greater than or equal to this value. Reads on small SSTables might be
+	// faster without a bloom filter.
+	MinFilterKeys uint32
+
+	// The minimum size a memtable needs to be before it is frozen and flushed to
+	// L0 object storage. Writes will still be flushed to the object storage WAL
+	// (based on flush_ms) regardless of this value. Memtable sizes are checked
+	// every `flush_ms` milliseconds.
+	//
+	// When setting this configuration, users must consider:
+	//
+	// * **Recovery time**: The larger the L0 SSTable size threshold, the less
+	//   frequently it will be written. As a result, the more recovery data there
+	//   will be in the WAL if a process restarts.
+	// * **Number of L0 SSTs/SRs**: The smaller the L0 SSTable size threshold, the
+	//   more SSTs and Sorted Runs there will be. L0 SSTables are not range
+	//   partitioned; each is its own sorted table. Similarly, each Sorted Run also
+	//   stores the entire keyspace. As such, reads that don't hit the WAL or memtable
+	//   may need to scan all L0 SSTables and Sorted Runs. The more there are, the
+	//   slower the scan will be.
+	// * **Memory usage**: The larger the L0 SSTable size threshold, the larger the
+	//   unflushed in-memory memtable will grow. This shouldn't be a concern for most
+	//   workloads, but it's worth considering for workloads with very high L0
+	//   SSTable sizes.
+	// * **API cost**: Smaller L0 SSTable sizes will result in more frequent writes
+	//   to object storage. This can increase your object storage costs.
+	// * **Secondary reader latency**: Secondary (non-writer) clients only see L0+
+	//   writes; they don't see WAL writes. Thus, the higher the L0 SSTable size, the
+	//   less frequently they will be written, and the longer it will take for
+	//   secondary readers to see new data.
+	L0SSTSizeBytes uint64
+
+	// Configuration options for the compactor.
+	CompactorOptions *CompactorOptions
+	CompressionCodec CompressionCodec
 }
 
 func DefaultDBOptions() DBOptions {
@@ -33,8 +88,16 @@ func DefaultDBOptions() DBOptions {
 
 type ReadLevel int
 
+// Whether reads see only writes that have been committed durably to the DB.  A
+// write is considered durably committed if all future calls to read are guaranteed
+// to serve the data written by the write, until some later durably committed write
+// updates the same key.
 const (
+	// Committed - Client reads will only see data that's been committed durably to the DB.
 	Committed ReadLevel = iota + 1
+
+	// Uncommitted - Clients will see all writes, including those not yet durably committed to the
+	// DB.
 	Uncommitted
 )
 
@@ -66,8 +129,14 @@ func DefaultWriteOptions() WriteOptions {
 }
 
 type CompactorOptions struct {
+	// The interval at which the compactor checks for a new manifest and decides
+	// if a compaction must be scheduled
 	PollInterval time.Duration
-	MaxSSTSize   uint64
+
+	// A compacted SSTable's maximum size (in bytes). If more data needs to be
+	// written to a Sorted Run during a compaction, a new SSTable will be created
+	// in the Sorted Run when this size is exceeded.
+	MaxSSTSize uint64
 }
 
 func DefaultCompactorOptions() *CompactorOptions {
