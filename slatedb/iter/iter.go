6013b6cf42307675a7036e9907d971a0b863af2e
Naveen <naveen246@gmail.com>
2024-10-10 19:56:49 +0530

    Add merge iterator

--- slatedb/iter/iter.go
+++ slatedb/iter/iter.go
@@ -1,14 +1,15 @@
-package common
+package iter
 
 import (
 	"github.com/samber/mo"
+	"github.com/slatedb/slatedb-go/slatedb/common"
 )
 
 type KVIterator interface {
 	// Next Returns the next non-deleted key-value pair in the iterator.
-	Next() (mo.Option[KV], error)
+	Next() (mo.Option[common.KV], error)
 
 	// NextEntry Returns the next entry in the iterator, which may be a key-value pair or
 	// a tombstone of a deleted key-value pair.
-	NextEntry() (mo.Option[KVDeletable], error)
+	NextEntry() (mo.Option[common.KVDeletable], error)
 }
